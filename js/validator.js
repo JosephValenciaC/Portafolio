e7c618690115aa3afbcf8cba084fee48798f7a45
Joseph <86898815+JosephValenciaC@users.noreply.github.com>
2021-12-17 14:50:15 -0600

    jvc

--- js/validator.js
+++ js/validator.js
@@ -1,13 +1,13 @@
-
-var response = grecaptcha.getResponse();
-
-if(response.length == 0)
-{
-    alert("Please verify that you are not a robot");
-    return false;
-}
-else{
-    return true;
-    
-}
-    
+$(document).ready(function() {
+    $('#entrar').click(function() {
+        grecaptcha.ready(function() {
+            grecaptcha.execute('reCAPTCHA_site_key', {
+                action: 'validarUsuario'
+                }).then(function(token) {
+                $('#form-login').prepend('<input type="hidden" name="token" value="' + token + '" >');
+                $('#form-login').prepend('<input type="hidden" name="action" value="validarUsuario" >');
+                $('#form-login').submit();
+            });
+        });
+    });
+});
